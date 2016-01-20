a70b87c49136da441ea12a888f2b9bee2f686848
Rafael Milleo Carrenho <rafaelmc@hdi.com.br>
2016-01-20 13:40:36 -0200

    Correção de rota, se usuário não está logado ele é redirecionado a tela de login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 module.exports = function(app){
-	app.get("/", function(req, res){
+	app.get("/", isLoggedIn, function(req, res){
 		var nweetModel = app.models.nweet;
 		
 		nweetModel.find().populate('autor').sort( [['data', 'descending']] ).limit(30).exec(function( error, nweets ){
 			res.render('index', {'session': req.session, "nweets": nweets});
 		});
 	});
+}
+
+
+var isLoggedIn = function(req, res, next){
+	if(typeof(req.session.usuario) == "undefined"){
+		res.redirect("/usuario/login");	
+	}
+	next();
 }
